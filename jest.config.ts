aa9e0351c9ac3dd5665088adfaebc4c82dd06bd6
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-11-21 16:52:06 +0900

    ㅁㅇㄹㅇㅁㄴ

--- jest.config.ts
+++ jest.config.ts
@@ -28,5 +28,4 @@ const config: Config = {
   coverageDirectory: "coverage",
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
