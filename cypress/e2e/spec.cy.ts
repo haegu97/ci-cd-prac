89147f800be433e9996cb20f33c19239de133121
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-11-22 09:24:37 +0900

    ㄹㅇㄹㅁ

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,5 @@
-describe("기본 테스트", () => {
-  it("홈페이지 방문", () => {
-    cy.visit("/");
-    cy.contains("Get started by editing");
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
   });
 });
